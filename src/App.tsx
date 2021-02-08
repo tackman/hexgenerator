18b7ce0137b3e199008e78b99e8f1783388905bf
tackman <tackman@tackman.info>
2021-02-08 15:47:25 +0900

    PixiJS installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Stage, Sprite } from '@inlet/react-pixi'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Stage>
+    <Sprite image="./logo512.png" x={100} y={100} />
+  </Stage>
+)
 
 export default App;
